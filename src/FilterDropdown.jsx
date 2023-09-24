c1b7811d7841998d752c6f08e794a4ad0e01c664
Carlo Askander <carloaskander@live.se>
2023-09-25 00:23:50 +0200

    Updated app and fixed issues with FilterDropdown

--- src/FilterDropdown.jsx
+++ src/FilterDropdown.jsx
@@ -1,33 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import CountryCard from './CountryCard';
+import React from 'react';
+import { useContext } from 'react';
+import { DarkModeContext } from './contexts/DarkModeContext';
+import './FilterDropdown.css';
 
-const CountryList = ({ searchTerm, filterRegion }) => {
-    // State to store all countries fetched from the API
-    const [countries, setCountries] = useState([]);
-
-    // Filtering countries based on search term and selected region
-    const filteredCountries = countries.filter(country => 
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase()) && // Match search term
-        (filterRegion === '' || country.region === filterRegion)               // Match region or show all if no region is selected
-    );
-
-    // UseEffect hook to fetch country data when the component mounts
-    useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags")
-        .then(response => response.json())
-        .then(data => {
-            setCountries(data); // Update state with fetched countries
-        });
-    }, []); // Empty dependency array means this useEffect runs once when the component mounts
+const FilterDropdown = ({ value, onValueChange }) => {
+    const { darkMode } = useContext(DarkModeContext);
+    const mode = darkMode ? 'dark' : 'light';
 
     return (
-        <div className="countries-grid">
-            {/* Map through filtered countries and render a CountryCard for each */}
-            {filteredCountries.map(country => (
-                <CountryCard key={country.cca3 || country.name.common} country={country} />
-            ))}
-        </div>
+        <select 
+            value={value} 
+            onChange={(e) => onValueChange(e.target.value)}
+            className={`filter-dropdown ${mode}`}
+        >
+            <option value="">Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="Americas">Americas</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+        </select>
     );
 }
 
-export default CountryList;
+export default FilterDropdown;
